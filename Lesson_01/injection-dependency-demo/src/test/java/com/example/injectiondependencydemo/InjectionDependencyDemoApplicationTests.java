be7961405af21b196f4d9bb1b5564bcff033e75f
qp133 <quangphung133@gmail.com>
2022-09-05 19:51:38 +0700

    update

---
+++ Lesson_01/injection-dependency-demo/src/test/java/com/example/injectiondependencydemo/InjectionDependencyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.injectiondependencydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InjectionDependencyDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

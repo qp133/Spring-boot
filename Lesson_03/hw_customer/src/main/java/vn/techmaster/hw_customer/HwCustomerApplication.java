9cdadb1d1f33c52c71175b27a205a2eb8e14dddb
qp133 <quangphung133@gmail.com>
2022-09-14 19:44:25 +0700

    update

---
+++ Lesson_03/hw_customer/src/main/java/vn/techmaster/hw_customer/HwCustomerApplication.java
@@ -0,0 +1,13 @@
+package vn.techmaster.hw_customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwCustomerApplication.class, args);
+	}
+
+}

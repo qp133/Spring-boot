ff60efafeea8303fb5e3b7f9ecd26d39b8334aec
qp133 <quangphung133@gmail.com>
2022-09-21 00:39:30 +0700

    update

--- Lesson_06/CarExercise/src/main/java/vn/techmaster/carexercise/entity/Car.java
+++ Lesson_06/CarExercise/src/main/java/vn/techmaster/carexercise/entity/Car.java
@@ -4,12 +4,21 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Car {
     private int id;
+
+    @NotBlank(message = "Brand cannot null")
+    @Email(message = "Not valid brand")
     private String brand;
+
+    @NotBlank(message = "Manufacturer cannot null")
+    @Email(message = "Not valid manufacturer")
     private String manufacturer;
     private String photo;
 }
